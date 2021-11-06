cb76a2ab4dd2f8012b4cf38ce06c699732f52fc4
Tim Eßer <essertim13@googlemail.com>
2021-11-06 15:55:36 +0100

    cleanup updatePlayerUI()

--- app/src/main/java/eu/time/betterspotify/controllers/PlayerController.kt
+++ app/src/main/java/eu/time/betterspotify/controllers/PlayerController.kt
@@ -21,7 +21,6 @@ import eu.time.betterspotify.activities.BigPlayerActivity
 import eu.time.betterspotify.activities.PlaylistActivity
 import eu.time.betterspotify.spotify.SpotifyApi
 import eu.time.betterspotify.util.*
-import java.util.*
 
 class PlayerController private constructor() {
     companion object {
@@ -154,7 +153,7 @@ class PlayerController private constructor() {
         sbProgress?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
                 if (fromUser) {
-                    updateTrackProgressText(activity)
+                    updateCurrentTrackProgressText(activity)
                 }
             }
 
@@ -187,25 +186,7 @@ class PlayerController private constructor() {
     private fun updatePlayerUI(context: Context, playerState: PlayerState) {
         val activity = context as Activity
 
-        val btnRepeat: ImageButton? = activity.findViewById(R.id.btnRepeat)
-        val btnShuffle: ImageButton? = activity.findViewById(R.id.btnShuffle)
-        val btnPlay: ImageButton? = activity.findViewById(R.id.btnPlay)
-        val btnLike: ImageButton? = activity.findViewById(R.id.btnLike)
-        val btnClose: ImageButton? = activity.findViewById(R.id.btnClose)
-        val btnOptions: ImageButton? = activity.findViewById(R.id.btnOptions)
-        val ivPlayerCover: ImageView? = activity.findViewById(R.id.ivPlayerCover)
         val tvPlayerTitle: TextView? = activity.findViewById(R.id.tvPlayerTitle)
-        val tvPlayerArtist: TextView? = activity.findViewById(R.id.tvPlayerArtist)
-        val pbProgress: ProgressBar? = activity.findViewById(R.id.pbProgress)
-        val sbProgress: SeekBar? = activity.findViewById(R.id.sbProgress)
-        val tvPlayerCurrentProgress: TextView? = activity.findViewById(R.id.tvPlayerCurrentProgress)
-        val tvPlayerMaxProgress: TextView? = activity.findViewById(R.id.tvPlayerMaxProgress)
-        val tvPlayerDevice: TextView? = activity.findViewById(R.id.tvPlayerDevice)
-        val tvPlayerContextTitle: TextView? = activity.findViewById(R.id.tvPlayerContextTitle)
-        val tvPlayerContextSubtitle: TextView? = activity.findViewById(R.id.tvPlayerContextSubtitle)
-        val vBackgroundBig: View? = activity.findViewById(R.id.vBackgroundBig)
-        val vBackgroundMini: View? = activity.findViewById(R.id.vBackgroundMini)
-
         val miniPlayer: View? = activity.findViewById(R.id.miniPlayer)
 
         context.runOnUiThread {
@@ -213,119 +194,198 @@ class PlayerController private constructor() {
             if (currentTrack != null && shouldUpdate(playerState)) {
                 miniPlayer?.visibility = View.VISIBLE
 
-                val trackDuration = currentTrack.duration
-                val trackProgress = playerState.playbackPosition
-
-                pbProgress?.max = trackDuration.toInt()
-                pbProgress?.progress = trackProgress.toInt()
-                sbProgress?.max = trackDuration.toInt()
-                sbProgress?.progress = trackProgress.toInt()
-
-                updateTrackProgressText(activity)
-                tvPlayerMaxProgress?.text = trackDuration.toTimestampString()
-
-                tvPlayerTitle?.isSelected = true
-
-//                Log.d("lastTrack", lastTrack.toString())
+                updateTrackProgress(activity, playerState)
+                updateButtonImages(activity, playerState)
 
                 if (currentTrack != lastTrack) {
-                    lastTrack = currentTrack
-
-                    if (tvPlayerContextTitle != null && tvPlayerContextSubtitle != null) {
-                        spotifyPlayer.getPlayerContext { playerContext ->
-                            tvPlayerContextTitle.text = playerContext.title
-                            tvPlayerContextSubtitle.text = playerContext.subtitle
-                        }
-                    }
-
-                    tvPlayerTitle?.text = currentTrack.name
-                    tvPlayerArtist?.text = currentTrack.getArtistsString()
-
-                    ivPlayerCover?.loadImageFromUri(currentTrack.imageUri) {
-                        if (vBackgroundBig != null) {
-                            val dominantColor = ivPlayerCover.getDominantColor()
-
-                            val gd = GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, intArrayOf(dominantColor, 0x000000, 0x000000))
-                            gd.cornerRadius = 0f
-                            vBackgroundBig.background = gd
-                            val contrastColor = getContrastColor(dominantColor)
-                            tvPlayerContextTitle?.setTextColor(contrastColor)
-                            tvPlayerContextSubtitle?.setTextColor(contrastColor)
-                            btnClose?.drawable?.setTint(contrastColor)
-                            btnOptions?.drawable?.setTint(contrastColor)
-                            activity.window.statusBarColor = dominantColor
-                            if (contrastColor == Color.WHITE) {
-                                activity.window.decorView.systemUiVisibility = activity.window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
-                            } else {
-                                activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-                            }
-                        }
-
-                        if (vBackgroundMini != null) {
-                            val dominantColor = ivPlayerCover.getDominantColor()
-
-                            val gd = GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, intArrayOf(dominantColor, 0x000000))
-                            gd.cornerRadius = 0f
-                            vBackgroundMini.background = gd
-                            val contrastColor = getContrastColor(dominantColor)
-                            tvPlayerTitle?.setTextColor(contrastColor)
-                            tvPlayerArtist?.setTextColor(contrastColor)
-                        }
-                    }
-
-                    if (btnLike != null) {
-                        spotifyPlayer.getLibraryState(currentTrack.uri) { libraryState ->
-                            if (libraryState.isAdded) {
-                                btnLike.setImageResource(R.drawable.ic_liked_48)
-                            } else {
-                                btnLike.setImageResource(R.drawable.ic_not_liked_48)
-                            }
-                        }
-                    }
-
-                    if (tvPlayerDevice != null) {
-                        SpotifyApi.getInstance().getPlayerState(context, { ownPlayerState ->
-                            tvPlayerDevice.text = ownPlayerState.device.name
-                        })
-                    }
+                    onTrackChange(activity, playerState)
                 }
 
-                if (btnPlay != null) {
-                    if (playerState.isPaused) {
-                        btnPlay.setImageResource(R.drawable.ic_baseline_play_arrow_48)
-                    } else {
-                        btnPlay.setImageResource(R.drawable.ic_baseline_pause_48)
-                    }
-                }
-
-                if (btnShuffle != null) {
-                    if (playerState.playbackOptions.isShuffling) {
-                        btnShuffle.setImageResource(R.drawable.ic_shuffle_on_48)
-                    } else {
-                        btnShuffle.setImageResource(R.drawable.ic_shuffle_off_48)
-                    }
-                }
-
-                if (btnRepeat != null) {
-                    when (playerState.playbackOptions.repeatMode) {
-                        0 -> {
-                            btnRepeat.setImageResource(R.drawable.ic_repeat_off_48)
-                        }
-                        1 -> {
-                            btnRepeat.setImageResource(R.drawable.ic_repeat_one_48)
-                        }
-                        2 -> {
-                            btnRepeat.setImageResource(R.drawable.ic_repeat_all_48)
-                        }
-                    }
-                }
+                tvPlayerTitle?.isSelected = true
             } else {
-//                miniPlayer.visibility = View.GONE
+                miniPlayer?.visibility = View.GONE
             }
         }
     }
 
-    private fun updateTrackProgressText(activity: Activity) {
+    private fun onTrackChange(activity: Activity, playerState: PlayerState) {
+        val currentTrack = playerState.track
+
+        lastTrack = currentTrack
+
+        updateTrackInfo(activity, currentTrack)
+        updateContextText(activity)
+        updateCover(activity)
+        updateDeviceInfo(activity)
+    }
+
+    private fun updateDeviceInfo(activity: Activity) {
+        val tvPlayerDevice: TextView? = activity.findViewById(R.id.tvPlayerDevice)
+
+        if (tvPlayerDevice != null) {
+            SpotifyApi.getInstance().getPlayerState(activity, { ownPlayerState ->
+                tvPlayerDevice.text = ownPlayerState.device.name
+            })
+        }
+    }
+
+    private fun updateTrackInfo(activity: Activity, currentTrack: Track) {
+        val tvPlayerTitle: TextView? = activity.findViewById(R.id.tvPlayerTitle)
+        val tvPlayerArtist: TextView? = activity.findViewById(R.id.tvPlayerArtist)
+
+        tvPlayerTitle?.text = currentTrack.name
+        tvPlayerArtist?.text = currentTrack.getArtistsString()
+
+        updateMaxTrackProgress(activity, currentTrack)
+    }
+
+    private fun updateContextText(activity: Activity) {
+        val tvPlayerContextTitle: TextView? = activity.findViewById(R.id.tvPlayerContextTitle)
+        val tvPlayerContextSubtitle: TextView? = activity.findViewById(R.id.tvPlayerContextSubtitle)
+
+        if (tvPlayerContextTitle != null && tvPlayerContextSubtitle != null) {
+            spotifyPlayer.getPlayerContext { playerContext ->
+                tvPlayerContextTitle.text = playerContext.title
+                tvPlayerContextSubtitle.text = playerContext.subtitle
+            }
+        }
+    }
+
+    private fun updateCover(activity: Activity) {
+        val ivPlayerCover: ImageView? = activity.findViewById(R.id.ivPlayerCover)
+
+        ivPlayerCover?.loadImageFromUri(lastTrack!!.imageUri) {
+            updateBackground(activity)
+        }
+    }
+
+    private fun updateBackground(activity: Activity) {
+        val vBackgroundBig: View? = activity.findViewById(R.id.vBackgroundBig)
+        val vBackgroundMini: View? = activity.findViewById(R.id.vBackgroundMini)
+
+        if (vBackgroundBig != null) {
+            updateBackgroundBig(activity, vBackgroundBig)
+        } else if (vBackgroundMini != null) {
+            updateBackgroundMini(activity, vBackgroundMini)
+        }
+    }
+
+    private fun updateBackgroundMini(activity: Activity, vBackgroundMini: View) {
+        val ivPlayerCover: ImageView = activity.findViewById(R.id.ivPlayerCover)
+        val tvPlayerTitle: TextView? = activity.findViewById(R.id.tvPlayerTitle)
+        val tvPlayerArtist: TextView? = activity.findViewById(R.id.tvPlayerArtist)
+
+        val dominantColor = ivPlayerCover.getDominantColor()
+
+        val gd = GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT, intArrayOf(dominantColor, 0x000000))
+        gd.cornerRadius = 0f
+        vBackgroundMini.background = gd
+        val contrastColor = getContrastColor(dominantColor)
+        tvPlayerTitle?.setTextColor(contrastColor)
+        tvPlayerArtist?.setTextColor(contrastColor)
+    }
+
+    private fun updateBackgroundBig(activity: Activity, vBackgroundBig: View) {
+        val ivPlayerCover: ImageView = activity.findViewById(R.id.ivPlayerCover)
+        val btnClose: ImageButton? = activity.findViewById(R.id.btnClose)
+        val btnOptions: ImageButton? = activity.findViewById(R.id.btnOptions)
+        val tvPlayerContextTitle: TextView? = activity.findViewById(R.id.tvPlayerContextTitle)
+        val tvPlayerContextSubtitle: TextView? = activity.findViewById(R.id.tvPlayerContextSubtitle)
+
+        val dominantColor = ivPlayerCover.getDominantColor()
+
+        val gd = GradientDrawable(GradientDrawable.Orientation.TOP_BOTTOM, intArrayOf(dominantColor, 0x000000, 0x000000))
+        gd.cornerRadius = 0f
+        vBackgroundBig.background = gd
+        val contrastColor = getContrastColor(dominantColor)
+        tvPlayerContextTitle?.setTextColor(contrastColor)
+        tvPlayerContextSubtitle?.setTextColor(contrastColor)
+        btnClose?.drawable?.setTint(contrastColor)
+        btnOptions?.drawable?.setTint(contrastColor)
+        activity.window.statusBarColor = dominantColor
+        if (contrastColor == Color.WHITE) {
+            activity.window.decorView.systemUiVisibility = activity.window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
+        } else {
+            activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+    }
+
+    private fun updateButtonImages(activity: Activity, playerState: PlayerState) {
+        val btnRepeat: ImageButton? = activity.findViewById(R.id.btnRepeat)
+        val btnShuffle: ImageButton? = activity.findViewById(R.id.btnShuffle)
+        val btnPlay: ImageButton? = activity.findViewById(R.id.btnPlay)
+        val btnLike: ImageButton? = activity.findViewById(R.id.btnLike)
+
+        if (btnPlay != null) {
+            if (playerState.isPaused) {
+                btnPlay.setImageResource(R.drawable.ic_baseline_play_arrow_48)
+            } else {
+                btnPlay.setImageResource(R.drawable.ic_baseline_pause_48)
+            }
+        }
+
+        if (btnShuffle != null) {
+            if (playerState.playbackOptions.isShuffling) {
+                btnShuffle.setImageResource(R.drawable.ic_shuffle_on_48)
+            } else {
+                btnShuffle.setImageResource(R.drawable.ic_shuffle_off_48)
+            }
+        }
+
+        if (btnRepeat != null) {
+            when (playerState.playbackOptions.repeatMode) {
+                0 -> {
+                    btnRepeat.setImageResource(R.drawable.ic_repeat_off_48)
+                }
+                1 -> {
+                    btnRepeat.setImageResource(R.drawable.ic_repeat_one_48)
+                }
+                2 -> {
+                    btnRepeat.setImageResource(R.drawable.ic_repeat_all_48)
+                }
+            }
+        }
+
+        if (btnLike != null) {
+            spotifyPlayer.getLibraryState(playerState.track.uri) { libraryState ->
+                if (libraryState.isAdded) {
+                    btnLike.setImageResource(R.drawable.ic_liked_48)
+                } else {
+                    btnLike.setImageResource(R.drawable.ic_not_liked_48)
+                }
+            }
+        }
+    }
+
+    private fun updateTrackProgress(activity: Activity, playerState: PlayerState) {
+        updateCurrentTrackProgress(activity, playerState)
+        updateCurrentTrackProgressText(activity)
+    }
+
+    private fun updateMaxTrackProgress(activity: Activity, currentTrack: Track) {
+        val pbProgress: ProgressBar? = activity.findViewById(R.id.pbProgress)
+        val sbProgress: SeekBar? = activity.findViewById(R.id.sbProgress)
+        val tvPlayerMaxProgress: TextView? = activity.findViewById(R.id.tvPlayerMaxProgress)
+
+        val trackDuration = currentTrack.duration
+
+        pbProgress?.max = trackDuration.toInt()
+        sbProgress?.max = trackDuration.toInt()
+        tvPlayerMaxProgress?.text = trackDuration.toTimestampString()
+    }
+
+    private fun updateCurrentTrackProgress(activity: Activity, playerState: PlayerState) {
+        val pbProgress: ProgressBar? = activity.findViewById(R.id.pbProgress)
+        val sbProgress: SeekBar? = activity.findViewById(R.id.sbProgress)
+        val trackProgress = playerState.playbackPosition
+
+        pbProgress?.progress = trackProgress.toInt()
+        sbProgress?.progress = trackProgress.toInt()
+
+        updateCurrentTrackProgressText(activity)
+    }
+
+    private fun updateCurrentTrackProgressText(activity: Activity) {
         val tvPlayerCurrentProgress: TextView? = activity.findViewById(R.id.tvPlayerCurrentProgress)
         val sbProgress: SeekBar? = activity.findViewById(R.id.sbProgress)
 
